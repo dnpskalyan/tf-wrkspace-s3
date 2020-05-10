eb1398424c55bf2540ec4f2c0747850e0bec29a8
kalyan <dnpskalyan83@hotmail.com>
2020-05-10 22:59:10 +0530

    code base

--- main.tf
+++ main.tf
@@ -1,73 +1,73 @@
 #This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    #access_key = "${var.aws_access_key}"
-    #secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+  #access_key = "${var.aws_access_key}"
+  #secret_key = "${var.aws_secret_key}"
+  region = "${var.aws_region}"
 }
 
 resource "aws_vpc" "default" {
-    cidr_block = "${var.vpc_cidr}"
-    enable_dns_hostnames = true
-    tags = {
-        Name = "${var.vpc_name}"
-    }
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.vpc_name}"
+  }
 }
 
 resource "aws_internet_gateway" "default" {
-    vpc_id = "${aws_vpc.default.id}"
-	tags = {
-        Name = "${var.IGW_name}"
-    }
+  vpc_id = "${aws_vpc.default.id}"
+  tags = {
+    Name = "${var.IGW_name}"
+  }
 }
 
 resource "aws_subnet" "subnet1-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet1_cidr}"
-    availability_zone = "us-east-1a"
+  vpc_id            = "${aws_vpc.default.id}"
+  cidr_block        = "${var.public_subnet1_cidr}"
+  availability_zone = "us-east-1a"
 
-    tags = {
-        Name = "${var.public_subnet1_name}"
-    }
+  tags = {
+    Name = "${var.public_subnet1_name}"
+  }
 }
 
 resource "aws_subnet" "subnet2-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet2_cidr}"
-    availability_zone = "us-east-1b"
+  vpc_id            = "${aws_vpc.default.id}"
+  cidr_block        = "${var.public_subnet2_cidr}"
+  availability_zone = "us-east-1b"
 
-    tags = {
-        Name = "${var.public_subnet2_name}"
-    }
+  tags = {
+    Name = "${var.public_subnet2_name}"
+  }
 }
 
 resource "aws_subnet" "subnet3-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet3_cidr}"
-    availability_zone = "us-east-1c"
+  vpc_id            = "${aws_vpc.default.id}"
+  cidr_block        = "${var.public_subnet3_cidr}"
+  availability_zone = "us-east-1c"
 
-    tags = {
-        Name = "${var.public_subnet3_name}"
-    }
-	
+  tags = {
+    Name = "${var.public_subnet3_name}"
+  }
+
 }
 
 
 resource "aws_route_table" "terraform-public" {
-    vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.default.id}"
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.default.id}"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.default.id}"
+  }
 
-    tags = {
-        Name = "${var.Main_Routing_Table}"
-    }
+  tags = {
+    Name = "${var.Main_Routing_Table}"
+  }
 }
 
 resource "aws_route_table_association" "terraform-public" {
-    subnet_id = "${aws_subnet.subnet1-public.id}"
-    route_table_id = "${aws_route_table.terraform-public.id}"
+  subnet_id      = "${aws_subnet.subnet1-public.id}"
+  route_table_id = "${aws_route_table.terraform-public.id}"
 }
 
 resource "aws_security_group" "allow_all" {
@@ -83,19 +83,26 @@ resource "aws_security_group" "allow_all" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-data "aws_ami" "my_ami" {
-     most_recent      = true
-     #name_regex       = "^mavrick"
-     owners           = ["721834156908"]
-}
+#data "aws_ami" "my_ami" {
+# most_recent = true
+#name_regex       = "^mavrick"
+# owners = ["721834156908"]
+#}
 
+terraform {
+  backend "s3" {
+    bucket = "devopsbucketb12"
+    key    = "myterraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 # resource "aws_instance" "web-1" {
 #     ami = "${data.aws_ami.my_ami.id}"
